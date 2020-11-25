84f8b00d53e15ee022d43d91002b92f169a7f125
Nadya <nadya01_95@mail.ru>
2020-11-25 22:25:50 +0700

    Реализация отправки формы

--- src/js/main.js
+++ src/js/main.js
@@ -1,103 +1,141 @@
 "use strict";
 
-// Меню навигации
+window.addEventListener('DOMContentLoaded', () => {
 
-const btnNav = document.querySelector('.header__btn-nav'),
-      nav = document.querySelector('.header__nav');
-  
-btnNav.addEventListener('click', () => {
-    if (nav.classList.contains('header__nav--closed')) {
-      nav.classList.remove('header__nav--closed');
-      nav.classList.add('header__nav--opened');
-    } else {
-      nav.classList.add('header__nav--closed');
-      nav.classList.remove('header__nav--opened');
+    // Меню навигации
+    const btnNav = document.querySelector('.header__btn-nav'),
+        nav = document.querySelector('.header__nav');
+
+    btnNav.addEventListener('click', () => {
+        if (nav.classList.contains('header__nav--closed')) {
+            nav.classList.remove('header__nav--closed');
+            nav.classList.add('header__nav--opened');
+        } else {
+            nav.classList.add('header__nav--closed');
+            nav.classList.remove('header__nav--opened');
+        }
+    });
+
+    // Форма на главной странице
+
+    // Функция отправки данных на сервер
+    const sendData = async (url, data) => {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: data
+        });
+
+        if (!response.ok) {
+            const err = new Error(`Адрес ошибки: ${url}, статус ошибки: ${response.status}`);
+            console.log(err);
+        }
+
+        return await response.json();
+    };
+
+    // Функция отправки данных с формы
+    const sendForm = (url, formSelector, inputSelectors, errorClass, activClassOfForm, selectorForMessageParent) => {
+        const form = document.querySelector(formSelector);
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const inputsOfForm = document.querySelectorAll(inputSelectors);
+
+            // Проверяем все ли импуты заполнены
+            let counter = 0;
+            inputsOfForm.forEach(input => {
+                if (!input.value) {
+                    input.classList.add(errorClass);
+                    counter++;
+                }
+
+                // Если инпут заполнен, убираем красную рамку
+                input.addEventListener('change', () => {
+                    if (input.value.length >= 1) {
+                        input.classList.remove(errorClass);
+                    }
+                });
+            });
+
+            if (counter == 0) {
+
+                // собираем все данные с формы и отправляем
+                const formData = new FormData(form);
+
+                sendData(url, formData)
+                    .then(() => {
+                        form.reset();
+                    })
+                    .catch(() => {
+                        const err = new Error(`Адрес ошибки: ${url}`);
+                        console.log(err);
+                    });
+
+                if (modalFormWrap.classList.contains(activClassOfForm)) {
+                    modalFormWrap.classList.remove(activClassOfForm);
+                }
+
+                // Попап при успешной отправке формы
+                const messageThanks = document.createElement('div');
+                messageThanks.classList.add('footer__modal-message');
+                messageThanks.innerHTML = `
+                <div class="modal__content">
+                    <div class="footer__modal-close" data-close>×</div>
+                    <div class="footer__modal-title">Спасибо, скоро мы с вами свяжемся!</div>
+                </div>
+            `;
+
+                document.querySelector(selectorForMessageParent).append(messageThanks);
+
+                setTimeout(() => {
+                    messageThanks.remove();
+                }, 5000);
+
+                document.querySelector('[data-close]').addEventListener('click', () => {
+                    messageThanks.remove();
+                });
+            }
+        });
+    };
+
+    const btnOpenForm = document.querySelector('.footer__btn-talk'),
+        modalFormWrap = document.querySelector('.footer__form-wrap');
+
+    if (btnOpenForm) {
+
+        btnOpenForm.addEventListener('click', () => {
+            modalFormWrap.classList.add('footer__form-wrap--show');
+        });
+
+        // Закрытие формы при клике за формой
+        modalFormWrap.addEventListener('click', (e) => {
+            if (e.target === modalFormWrap) {
+                modalFormWrap.classList.remove('footer__form-wrap--show');
+            }
+        });
+
+        sendForm('https://jsonplaceholder.typicode.com/posts',
+            '.footer__form',
+            '[data-inputForm = "footer"]',
+            'form__input--error',
+            'footer__form-wrap--show',
+            '.footer'
+        );
+    }
+
+    const contactForm = document.querySelector('.contact-center__form');
+
+    if (contactForm) {
+        sendForm('https://jsonplaceholder.typicode.com/posts',
+            '.contact-center__form',
+            '[data-inputForm = "contact-center"]',
+            'form__input--error',
+            'footer__form-wrap--show',
+            '.contact-center'
+        );
     }
 });
 
-// Форма на главной странице
 
-const btnOpenForm = document.querySelector('.footer__btn-talk'),
-      modalFormWrap = document.querySelector('.footer__form-wrap'),
-      modalForm = document.querySelector('.footer__form'),
-      formName = document.querySelector('#name'),
-      formEmail = document.querySelector('#email'),
-      formMessage = document.querySelector('#message');
-
-if (btnOpenForm) {
-
-  btnOpenForm.addEventListener('click', () => {
-    modalFormWrap.classList.add('footer__form-wrap--show');
-  });
-
-  // Закрытие формы при клике за формой
-  modalFormWrap.addEventListener('click', (e) => {
-      if (e.target === modalFormWrap) {
-          modalFormWrap.classList.remove('footer__form-wrap--show');
-      }
-  });
-
-  modalForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    //  Проверяем все ли инпуты заполнены
-    if (!formEmail.value || !formMessage.value || !formName.value) {
-      if (!formEmail.value) {
-        formEmail.classList.add('form__input--error');
-      }
-
-      if (!formMessage.value) {
-        formMessage.classList.add('form__input--error');
-      }
-
-      if (!formName.value) {
-        formName.classList.add('form__input--error');
-      } 
-
-      } else {
-
-      modalFormWrap.classList.remove('footer__form-wrap--show');
-
-      // Попап при успешной отправке формы
-      const messageTanks = document.createElement('div');
-      messageTanks.classList.add('footer__modal-message');
-      messageTanks.innerHTML = `
-          <div class="modal__content">
-              <div class="footer__modal-close" data-close>×</div>
-              <div class="footer__modal-title">Спасибо, скоро мы с вами свяжемся!</div>
-          </div>
-      `;
-    
-      document.querySelector('.footer').append(messageTanks);
-    
-      setTimeout(() => {
-        messageTanks.remove();
-      }, 5000);
-    
-      document.querySelector('[data-close]').addEventListener('click', () => {
-        messageTanks.remove();
-      });
-    }
-
-    // При заполнение инпутов убираем красный бордер
-    formName.addEventListener("change", function() {
-      if(formName.value.length >= 1) {
-        formName.classList.remove("form__input--error");
-      }
-    });
-
-    formEmail.addEventListener("change", function() {
-      if(formEmail.value.length >= 1) {
-        formEmail.classList.remove("form__input--error");
-      }
-    });
-
-    formMessage.addEventListener("change", function() {
-      if(formMessage.value.length >= 1) {
-        formMessage.classList.remove("form__input--error");
-      }
-    });
-
-  });
-}
 
